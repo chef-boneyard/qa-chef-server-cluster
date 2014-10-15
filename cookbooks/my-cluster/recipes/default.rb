c6cbf5ad0da93177fa00cba9e329280e1944b7f7
Patrick Wright <patrick@getchef.com>
2014-10-15 13:24:34 -0700

    update attributes

--- cookbooks/my-cluster/recipes/default.rb
+++ cookbooks/my-cluster/recipes/default.rb
@@ -4,4 +4,3 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
-Chef::Log.info "metal-provisioner-key-name: #{node.default['chef-server-cluster']['aws']['machine_options']['bootstrap_options']['key_name']}"
