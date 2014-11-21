13b775c517fc2ae7987866f189bab33b4adc823e
Patrick Wright <patrick@getchef.com>
2014-11-21 14:10:16 -0700

    update readme

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,5 @@ key_name = 'wrightp-metal-provisioner'
 private_keys key_name => '/tmp/ssh/id_rsa'
 public_keys  key_name => '/tmp/ssh/id_rsa.pub'
 chef_repo_path File.join(current_dir, '..')
-cookbook_path [ File.join(current_dir, '..'),
-                File.join(current_dir, '..', 'berks-cookbooks') ]
+cookbook_path [ File.join(current_dir, '..', 'berks-cookbooks') ]
 cache_path File.join(current_dir, 'local-mode-cache')
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-csc-repo
+qa-chef-server-cluster
 ========
 
 Until more easily configurable set the key names in:
 * .chef/knife.rb
 * qa-chef-server-cluster cookbook attributes file
+* [Setup ssh keys data bag](https://github.com/opscode-cookbooks/chef-server-cluster#create-a-secrets-data-bag-and-populate-it-with-the-ssh-keys)
 
 All hardcoded to run standalone for now
 
 `rake -T`
-
