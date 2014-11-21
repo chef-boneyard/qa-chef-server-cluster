4ff127876933f5ae6c22e40c59d0c5837732f9c1
Patrick Wright <patrick@getchef.com>
2014-11-21 14:51:50 -0700

    forget requires

--- recipes/standalone-clean.rb
+++ recipes/standalone-clean.rb
@@ -1,3 +1,5 @@
+require 'chef/provisioning'
+
 machine node['qa-chef-server-cluster']['standlone']['machine-name'] do
   action :destroy
 end
--- recipes/standalone-test.rb
+++ recipes/standalone-test.rb
@@ -1,3 +1,5 @@
+require 'chef/provisioning'
+
 machine_execute 'chef-server-ctl test' do
   machine node['qa-chef-server-cluster']['standlone']['machine-name']
 end
