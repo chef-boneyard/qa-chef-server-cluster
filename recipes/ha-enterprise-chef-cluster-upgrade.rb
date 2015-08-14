8997a4b8d2bfb95c0dd8f5e16fd4ff370a937d02
Patrick Wright <patrick@getchef.com>
2015-08-14 10:39:19 -0700

    ec upgrade

--- recipes/ha-enterprise-chef-cluster-upgrade.rb
+++ recipes/ha-enterprise-chef-cluster-upgrade.rb
@@ -43,31 +43,22 @@ machine_batch do
 end
 
 machine node['bootstrap-backend'] do
-  run_list [ 'qa-chef-server-cluster::ha-upgrade-stop-all-services' ]
-end
-
-machine node['bootstrap-backend'] do
-  run_list [ 'qa-chef-server-cluster::ha-upgrade-exec' ]
+  run_list [ 'qa-chef-server-cluster::ha-upgrade-stop-all-services',
+             'qa-chef-server-cluster::ha-upgrade-exec' ]
 end
 
 download_bootstrap_files
 
 machine_batch do
   machine node['frontend'] do
+    run_list [ 'qa-chef-server-cluster::ha-upgrade-exec' ]
     files node['qa-chef-server-cluster']['chef-server']['files']
   end
 
   machine node['secondary-backend'] do
+    run_list [ 'qa-chef-server-cluster::ha-upgrade-exec' ]
     files node['qa-chef-server-cluster']['chef-server']['files']
   end
-end
-
-machine node['secondary-backend'] do
-  run_list [ 'qa-chef-server-cluster::ha-upgrade-exec' ]
-end
-
-machine node['frontend'] do
-  run_list [ 'qa-chef-server-cluster::ha-upgrade-exec' ]
 end
 
 machine_batch do
@@ -84,6 +75,7 @@ machine_batch do
   machine node['bootstrap-backend'] do
     run_list [ 'qa-chef-server-cluster::ha-verify-backend-master' ]
   end
+
   machine node['secondary-backend'] do
     run_list [ 'qa-chef-server-cluster::ha-verify-backend-backup' ]
   end
