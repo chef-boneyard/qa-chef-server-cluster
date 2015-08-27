ef0f608ef5e70c8114547ed4af8ee12fdb37fb95
Patrick Wright <patrick@getchef.com>
2015-08-26 21:26:24 -0700

    bump cookbok version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Patrick Wright'
 maintainer_email 'patrick@chef.io'
 license          'all_rights'
 description      'Installs/Configures QA clusters'
-version          '2.1.0'
+version          '2.1.1'
 
 depends 'chef-ingredient'
 depends 'omnibus-artifactory-artifact'
