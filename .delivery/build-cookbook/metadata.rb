d2ce71aeaa711691de1a3c4638aa863e5034475a
Patrick Wright <patrick@getchef.com>
2015-08-26 21:10:00 -0700

    add lint, syntax, unit

---
+++ .delivery/build-cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name             'qa-chef-server-cluster-acceptance'
+maintainer       'Patrick Wright'
+maintainer_email 'patrick@chef.io'
+license          'all_rights'
+description      'Installs/Configures build'
+long_description 'Installs/Configures build'
+version          '0.1.0'
+
+depends 'delivery-truck'
