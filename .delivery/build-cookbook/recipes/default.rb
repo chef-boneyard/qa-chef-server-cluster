7f2b88df118e0944ce8dca4806140658d3bc5a30
Patrick Wright <patrick@getchef.com>
2015-08-27 15:14:36 -0700

    add delivery_build

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -17,24 +17,6 @@
 
 include_recipe 'delivery-truck'
 
-delivery_github 'chef/qa-chef-server-cluster' do
-  deploy_key secrets['github']
-  branch node['delivery']['change']['pipeline']
-  remote_url "git@github.com:#{github_repo}.git"
-  repo_path node['delivery']['workspace']['repo']
-  cache_path node['delivery']['workspace']['cache']
-  action :clone
-end
-
-delivery_github 'opscode-cookbooks/omnibus-artifactory-artifact' do
-  deploy_key secrets['github']
-  branch node['delivery']['change']['pipeline']
-  remote_url "git@github.com:#{github_repo}.git"
-  repo_path node['delivery']['workspace']['repo']
-  cache_path node['delivery']['workspace']['cache']
-  action :clone
-end
-
 chef_gem 'mixlib-versioning' do
   compile_time false
   action :install
