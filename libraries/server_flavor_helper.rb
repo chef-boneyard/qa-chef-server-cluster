8b1274d8e0cc37050e287d0c48e318a9d9b76e0a
Patrick Wright <patrick@getchef.com>
2015-10-15 16:57:53 -0700

    remove instance vars

--- libraries/server_flavor_helper.rb
+++ libraries/server_flavor_helper.rb
@@ -13,7 +13,7 @@ module QaChefServerCluster
 
   module ServerFlavorHelper
     def open_source_chef_config
-      @open_source_chef = ChefServer.new(
+      ChefServer.new(
         product_name: 'open_source_chef',
         ctl_exec: 'chef-server-ctl',
         config_file: 'chef-server.rb',
@@ -23,7 +23,7 @@ module QaChefServerCluster
     end
 
     def chef_server_config
-      @chef_server = ChefServer.new(
+      ChefServer.new(
         product_name: 'chef_server',
         ctl_exec: 'chef-server-ctl',
         config_file: 'chef-server.rb',
@@ -33,7 +33,7 @@ module QaChefServerCluster
     end
 
     def enterprise_chef_config
-      @enterprise_chef = ChefServer.new(
+      ChefServer.new(
         product_name: 'enterprise_chef',
         ctl_exec: 'private-chef-ctl',
         config_file: 'private-chef.rb',
