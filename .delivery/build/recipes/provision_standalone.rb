976eabaa7e47b205e860c051eb02e8f04d72a61f
Patrick Wright <patrick@getchef.com>
2015-10-14 17:40:16 -0700

    repo config.rb

--- .delivery/build/recipes/provision_standalone.rb
+++ .delivery/build/recipes/provision_standalone.rb
@@ -9,12 +9,13 @@ cache = node['delivery']['workspace']['cache']
 qa_path = "#{path}/deps/qa-chef-server-cluster"
 attributes_install_file = File.join(cache, 'install.json')
 repo_knife_file = File.join(qa_path, '.chef/knife.rb')
+repo_config_file = File.join(path, '.chef/config.rb')
 
 ruby_block 'stand-up-machine' do
   block do
     #Dir.chdir qa_path
     Dir.chdir path
     #shell_out_retry("bundle exec chef-client -z -p 10257 -j #{attributes_install_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server --force-formatter")
-    shell_out_retry("bundle exec chef-client -z -p 10257 -j #{attributes_install_file} -o qa-chef-server-cluster::standalone-server --force-formatter")
+    shell_out_retry("bundle exec chef-client -z -p 10257 -j #{attributes_install_file} -c #{repo_config_file} -o qa-chef-server-cluster::standalone-server --force-formatter")
   end
 end
