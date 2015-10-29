733e0f0aeb97fd59f4714bda623145e9d67d027a
Patrick Wright <patrick@getchef.com>
2015-10-28 20:45:41 -0700

    standalone clean smoke

--- .delivery/build/recipes/smoke_general_prep.rb
+++ .delivery/build/recipes/smoke_general_prep.rb
@@ -8,9 +8,8 @@ include_recipe 'build::prepare_deps'
 include_recipe 'build::prepare_acceptance'
 
 path = node['delivery']['workspace']['repo']
-qa_path = "#{path}/deps/qa-chef-server-cluster"
 
 # and then write it back to disk where qa-chef-server-cluster expects it
-directory File.join(qa_path, 'nodes') do
+directory File.join(path, '.chef', 'nodes') do
   action :create
 end
