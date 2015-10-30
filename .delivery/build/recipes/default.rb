0d5cbde24e910693583260958f7632467f7cc10c
Patrick Wright <patrick@getchef.com>
2015-10-30 10:36:01 -0700

    move gem_cache setup

--- .delivery/build/recipes/default.rb
+++ .delivery/build/recipes/default.rb
@@ -4,20 +4,6 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-
-# get to the qa-chef-server-cluster project root and use it as a cache
-# as it is persistent between build jobs
-node.override['gem_cache'] = File.join(node['delivery']['workspace']['root'], "../../../project_gem_cache")
-
-# TODO: remove directory gem_cache do
-directory node['gem_cache'] do
-  # set the owner to the dbuild so that the other recipes can write to here
-  owner node['delivery_builder']['build_user']
-  mode "0755"
-  recursive true
-  action :create
-end
-
 include_recipe 'delivery-sugar-extras::default'
 include_recipe 'delivery-red-pill::default'
 include_recipe 'delivery-truck::default'
--- .delivery/build/recipes/prepare_deps.rb
+++ .delivery/build/recipes/prepare_deps.rb
@@ -9,7 +9,19 @@
 repo = node['delivery']['workspace']['repo']
 install_dir = '/opt/chefdk'
 
-execute "bundle install --path=#{node['gem_cache']}" do
+# get to the project root and use it as a cache
+# as it is persistent between build jobs
+gem_cache = File.join(node['delivery']['workspace']['root'], "../../../project_gem_cache")
+
+directory gem_cache do
+  # set the owner to the dbuild so that the other recipes can write to here
+  owner node['delivery_builder']['build_user']
+  mode '0755'
+  recursive true
+  action :create
+end
+
+execute "bundle install --path=#{gem_cache}" do
   cwd repo
   environment(
     'PATH' => node['chef-server-acceptance']['delivery-path'],
