c471f7849a322d13da5b641384daf20277a29b77
Patrick Wright <patrick@getchef.com>
2015-10-30 10:43:04 -0700

    reset gem cache

--- .delivery/build/recipes/default.rb
+++ .delivery/build/recipes/default.rb
@@ -4,6 +4,18 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+# get to the project root and use it as a cache
+# as it is persistent between build jobs
+gem_cache = File.join(node['delivery']['workspace']['root'], "../../../project_gem_cache")
+
+directory gem_cache do
+  # set the owner to the dbuild so that the other recipes can write to here
+  owner node['delivery_builder']['build_user']
+  mode '0755'
+  recursive true
+  action :create
+end
+
 include_recipe 'delivery-sugar-extras::default'
 include_recipe 'delivery-red-pill::default'
 include_recipe 'delivery-truck::default'
