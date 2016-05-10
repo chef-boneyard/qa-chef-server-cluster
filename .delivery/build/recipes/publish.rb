c66671988697d80e236f8921e275c4e0d1af443a
Patrick Wright <patrick@getchef.com>
2016-05-10 11:01:07 -0600

    uncomment publish recipe

--- .delivery/build/recipes/publish.rb
+++ .delivery/build/recipes/publish.rb
@@ -4,16 +4,16 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-#include_recipe "delivery-truck::publish"
+include_recipe "delivery-truck::publish"
 
-#secrets = get_project_secrets
-#github_repo = node['delivery']['config']['delivery-truck']['publish']['github']
-#
-#delivery_github github_repo do
-#  deploy_key secrets['github']
-#  branch node['delivery']['change']['pipeline']
-#  remote_url "git@github.com:#{github_repo}.git"
-#  repo_path node['delivery']['workspace']['repo']
-#  cache_path node['delivery']['workspace']['cache']
-#  action :push
-#end
+secrets = get_project_secrets
+github_repo = node['delivery']['config']['delivery-truck']['publish']['github']
+
+delivery_github github_repo do
+ deploy_key secrets['github']
+ branch node['delivery']['change']['pipeline']
+ remote_url "git@github.com:#{github_repo}.git"
+ repo_path node['delivery']['workspace']['repo']
+ cache_path node['delivery']['workspace']['cache']
+ action :push
+end
