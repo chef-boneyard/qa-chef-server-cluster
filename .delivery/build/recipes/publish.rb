e8499af73d64d01b7f412b3c42beeae57cf9d33d
Salim Afiune <afiune@chef.io>
2016-06-15 18:22:18 -0400

    Enable publish to github

--- .delivery/build/recipes/publish.rb
+++ .delivery/build/recipes/publish.rb
@@ -4,7 +4,4 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-#include_recipe "delivery-truck::publish" if node['delivery']['change']['pipeline'] == 'master'
-#
-
-puts node['delivery']
+include_recipe "delivery-truck::publish" if node['delivery']['change']['pipeline'] == 'master'
