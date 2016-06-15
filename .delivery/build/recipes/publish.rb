f5c4dbcd1980e941eea53af05f0aaf2ca65a8309
Salim Afiune <afiune@chef.io>
2016-06-15 16:46:51 -0400

    Disable publish for troubleshooting

--- .delivery/build/recipes/publish.rb
+++ .delivery/build/recipes/publish.rb
@@ -4,4 +4,7 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-include_recipe "delivery-truck::publish" if node['delivery']['change']['pipeline'] == 'master'
+#include_recipe "delivery-truck::publish" if node['delivery']['change']['pipeline'] == 'master'
+#
+
+puts node['delivery']
