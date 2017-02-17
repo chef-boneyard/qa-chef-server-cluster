8779d21b8c67b244ab76d2a0adf274c0b5a6c5db
Stephan Renatus <srenatus@chef.io>
2017-02-17 11:39:09 +0100

    use more recent package

--- .delivery/build/attributes/default.rb
+++ .delivery/build/attributes/default.rb
@@ -44,11 +44,9 @@ default['chef_server_instance_size'] = 'm3.medium'
 
 # These set the version of the Chef Server that we intend to test.
 default['chef_server_test_flavor'] = 'chef_server'
-default['chef_server_test_version'] = 'latest'
-default['chef_server_test_channel'] = 'unstable'
-#testing comment
-#current
-default['chef_server_test_url_override'] = 'https://packages.chef.io/files/unstable/chef-server/12.12.0+20170216163121/ubuntu/14.04/chef-server-core_12.12.0+20170216163121-1_amd64.deb'
+# default['chef_server_test_version'] = 'latest'
+# default['chef_server_test_channel'] = 'unstable'
+default['chef_server_test_url_override'] = 'https://packages.chef.io/files/unstable/chef-server/12.12.0+20170217100119/ubuntu/14.04/chef-server-core_12.12.0+20170217100119-1_amd64.deb'
 
 # Us a specific target by URL instead:
 # default['chef_server_test_url_override'] = 'https://packages.chef.io/files/unstable/chef-server/12.11.2+20170127181122/ubuntu/14.04/chef-server-core_12.11.2+20170127181122-1_amd64.deb'
