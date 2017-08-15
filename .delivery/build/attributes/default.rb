5f1b19841ef4c831c0c0a85ba5460f67122f50f3
Steven Danna <steve@chef.io>
2017-08-15 20:55:08 +0100

    Test 12.16 build
    Signed-off-by: Steven Danna <steve@chef.io>

--- .delivery/build/attributes/default.rb
+++ .delivery/build/attributes/default.rb
@@ -16,21 +16,21 @@ default['chef-server-acceptance']['delivery-path'] ='/opt/chefdk/embedded/bin:/u
 if node['delivery']['change']['stage'] == 'acceptance'
   default['delivery-matrix']['acceptance']['matrix'] = [
    # fresh install of chef_server_version
-   'standalone_clean_aws'
-   # 'tier_clean_aws',
-   # 'ha_clean_aws',
+   'standalone_clean_aws',
+   'tier_clean_aws',
+   'ha_clean_aws',
 
    # chef_server_latest_released_version > chef_server_version upgrade testing
-   # 'standalone_upgrade_aws',
-   # 'tier_upgrade_aws',
-   # 'ha_upgrade_aws',
+   'standalone_upgrade_aws',
+   'tier_upgrade_aws',
+   'ha_upgrade_aws',
 
    # OSC 11.latest > chef_server_version upgrade testing (standalone only)
-    # 'standalone_osc_upgrade_aws',
+   'standalone_osc_upgrade_aws',
 
    #  # EC 11.latest > chef_server_version upgrade testing
-   # 'standalone_ec_upgrade_aws',
-   # 'tier_ec_upgrade_aws',
+   'standalone_ec_upgrade_aws',
+   'tier_ec_upgrade_aws'
    # Commented out because it is so unreliable as to tell us nothing useful.
    # 'ha_ec_upgrade_aws'
   ]
@@ -56,8 +56,7 @@ default['chef_server_test_flavor'] = 'chef_server'
 #default['chef_server_test_channel'] = 'unstable'
 
 # Us a specific target by URL instead:
-
-default['chef_server_test_url_override'] = 'https://packages.chef.io/files/unstable/chef-server/12.15.8+20170627085616/ubuntu/14.04/chef-server-core_12.15.8+20170627085616-1_amd64.deb'
+default['chef_server_test_url_override'] = 'https://packages.chef.io/files/current/chef-server/12.16.2/ubuntu/14.04/chef-server-core_12.16.2-1_amd64.deb'
 
 #
 # Source Release
