cbe1774d74affb025d096d86994da68d6ed7595b
Steven Danna <steve@chef.io>
2017-10-25 10:47:10 +0100

    Test 12.17.5 for release
    Signed-off-by: Steven Danna <steve@chef.io>

--- .delivery/build/attributes/default.rb
+++ .delivery/build/attributes/default.rb
@@ -57,7 +57,7 @@ default['chef_server_test_flavor'] = 'chef_server'
 #default['chef_server_test_channel'] = 'unstable'
 
 # Us a specific target by URL instead:
-default['chef_server_test_url_override'] = 'https://packages.chef.io/files/current/chef-server/12.17.3/ubuntu/14.04/chef-server-core_12.17.3-1_amd64.deb'
+default['chef_server_test_url_override'] = 'https://packages.chef.io/files/current/chef-server/12.17.5/ubuntu/14.04/chef-server-core_12.17.5-1_amd64.deb'
 
 #
 # Source Release
