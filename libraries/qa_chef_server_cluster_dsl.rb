c3fe022b411773d330c18952f5197cc9d8407613
Ryan Hass <rhass@users.noreply.github.com>
2018-02-22 16:09:37 -0800

    Add machine name and action to for migration-level.

--- libraries/qa_chef_server_cluster_dsl.rb
+++ libraries/qa_chef_server_cluster_dsl.rb
@@ -76,6 +76,8 @@ module QaChefServerCluster
         local_path File.join(
           node['qa-chef-server-cluster']['chef-server']['file-dir'],
           'migration-level'
+          machine machine_name
+          action :download
         )
       end
     end
