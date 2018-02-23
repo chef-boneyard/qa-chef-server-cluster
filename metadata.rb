fcda791ee421a77e5cc2fd6e31cd4f97b0bc8fab
Ryan Hass <rhass@users.noreply.github.com>
2018-02-22 16:11:50 -0800

    Bump cookbook version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Patrick Wright'
 maintainer_email 'patrick@chef.io'
 license          'all_rights'
 description      'Installs/Configures QA clusters'
-version          '0.1.36'
+version          '0.1.37'
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
 depends 'chef-ingredient'
